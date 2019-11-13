58efded7fe1e73749057216891f1d285db2a650f
Arkadiusz Plichta <el.pikel@gmail.com>
2019-11-13 11:17:34 +0100

    clean only queues that have messages

--- clean_queues.exs
+++ clean_queues.exs
@@ -1,31 +1,34 @@
 #! /usr/bin/env elixir
 defmodule CleanQueues do
   @doc """
-    Cleans queues starting with `q.v1.`
+    Cleans queues starting with `q.v1.`. As an arument plesea provide full path to rabbitmqctl
+    for example : `/usr/local/opt/rabbitmq/sbin/rabbitmqctl`
   """
-  def clean() do
-    get_queues()
-    |> clean_queues()
+  def clean(rabbitmqctl) do
+    rabbitmqctl
+    |> get_queues()
+    |> clean_queues(rabbitmqctl)
   end
 
-  defp get_queues() do
-    {queues, _} = System.cmd("/usr/local/opt/rabbitmq/sbin/rabbitmqctl", ["list_queues"])
+  defp get_queues(rabbitmqctl) do
+    {queues, _} = System.cmd(rabbitmqctl, ["list_queues"])
 
     queues
     |> String.split("\n")
-    |> Enum.filter(fn queue -> String.starts_with?(queue, "q.v1.") end)
+    |> Enum.filter(fn queue -> String.contains?(queue, ".q.") end)
+    |> Enum.filter(fn queue -> String.split(queue, "\t") |> Enum.at(1) != "0" end)
     |> Enum.map(fn queue -> String.split(queue, "\t") |> Enum.at(0) end)
   end
 
-  defp clean_queues(queues) do
+  defp clean_queues(queues, rabbitmqctl) do
     queues
-    |> Enum.each(fn queue -> clean_queue(queue) end)
+    |> Enum.each(fn queue -> clean_queue(queue, rabbitmqctl) end)
   end
 
-  defp clean_queue(queue) do
+  defp clean_queue(queue, rabbitmqctl) do
     IO.puts("Purging: " <> queue)
-    System.cmd("/usr/local/opt/rabbitmq/sbin/rabbitmqctl", ["purge_queue", String.trim(queue)])
+    System.cmd(rabbitmqctl, ["purge_queue", String.trim(queue)])
   end
 end
 
-CleanQueues.clean()
+CleanQueues.clean(System.argv() |> Enum.at(0) || "/usr/local/opt/rabbitmq/sbin/rabbitmqctl")
